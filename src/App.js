4e152c4c98166ed5753a48d411e12b009d00b955
LeishaH <210328815+LeishaH@users.noreply.github.com>
2026-04-01 13:52:25 +1100

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -2,5 +2,29 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <a
+        href="https://github.com/LeishaH/react-weather"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        View on GitHub
+      </a>
+
+      <footer>
+        This project was coded by Leisha Henry and is{" "}
+        <a
+          href="https://github.com/LeishaH/react-weather"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
