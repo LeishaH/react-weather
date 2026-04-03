8b8bc46c491588cd792783c7f36efd5e158daafa
LeishaH <210328815+LeishaH@users.noreply.github.com>
2026-04-03 16:27:48 +1100

    Button Added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,25 @@
-export default function App() {
-  return <h1>HELLO THIS IS NEW</h1>;
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <button className="btn btn-primary">Test</button>
+
+      <footer>
+        This project was coded by Leisha Henry and is{" "}
+        <a
+          href="https://github.com/LeishaH/react-weather"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+        .
+      </footer>
+    </div>
+  );
 }
+
+export default App;
