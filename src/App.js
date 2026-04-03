ab5e9a523cdb94667933aae979d7ba5c83b947c4
LeishaH <210328815+LeishaH@users.noreply.github.com>
2026-04-03 18:35:57 +1100

    Fixed App.js and added Bootstrap import

--- src/App.js
+++ src/App.js
@@ -7,11 +7,13 @@ export default function App() {
       <div className="container text-center mt-5">
         <h1 className="mb-4">Weather App</h1>
 
+        <button className="btn btn-danger mb-3">TEST BUTTON</button>
+
         <footer>
           <p>This project was coded by Leisha Henry and is</p>
 
           <a
-            href="https://github.com/LeishaH/react-weather"
+            href="https://github.com/LeishaH/react-weather-clean"
             target="_blank"
             rel="noopener noreferrer"
             className="btn btn-primary"
