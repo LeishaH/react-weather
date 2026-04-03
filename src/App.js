a22ecb300a753fd99d56299e1dc37adace0fd3e2
LeishaH <210328815+LeishaH@users.noreply.github.com>
2026-04-03 16:24:49 +1100

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,22 +1,3 @@
-import React from "react";
-import "./App.css";
-
 export default function App() {
-  return (
-    <div className="App">
-      <h1>Weather App</h1>
-
-      <footer>
-        This project was coded by Leisha Henry and is{" "}
-        <a
-          href="https://github.com/LeishaH/react-weather"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-        .
-      </footer>
-    </div>
-  );
+  return <h1>HELLO THIS IS NEW</h1>;
 }
